c9d2ab94833e5648007a0e1602d4f50963f97729
pugud19 <noerbayu73@gmail.com>
2022-02-22 09:33:44 +0700

    edit some file and fix change password

--- resources/views/loker.blade.php
+++ resources/views/loker.blade.php
@@ -9,27 +9,27 @@
             </div>
             <div class="w-full">
             <div class="font-semibold">
-            {{ Auth::user()->name }} 
+            {{ Auth::user()->name }}
             </div>
              <div class="my-5">
-                
-                <textarea class="form-textarea p-2 w-full border-gray-300 h-10 rounded-lg resize-none overflow-hidden focus:border-blue-100 focus:ring focus:ring-blue-200 transition duration-100 " 
+
+                <textarea class="form-textarea p-2 w-full border-gray-300 h-10 rounded-lg resize-none overflow-hidden focus:border-blue-100 focus:ring focus:ring-blue-200 transition duration-100 "
                name="name" id="name" placeholder="Nama perusahaan "></textarea>
-               <textarea class="form-textarea p-2 w-full border-gray-300 h-10 rounded-lg resize-none overflow-hidden focus:border-blue-100 focus:ring focus:ring-blue-200 transition duration-100 " 
+               <textarea class="form-textarea p-2 w-full border-gray-300 h-10 rounded-lg resize-none overflow-hidden focus:border-blue-100 focus:ring focus:ring-blue-200 transition duration-100 "
                name="bagian" id="bagian" placeholder="Bagian pekerjaan"></textarea>
-               <textarea class="form-textarea p-2 w-full border-gray-300 h-10 rounded-lg resize-none overflow-hidden focus:border-blue-100 focus:ring focus:ring-blue-200 transition duration-100 " 
+               <textarea class="form-textarea p-2 w-full border-gray-300 h-10 rounded-lg resize-none overflow-hidden focus:border-blue-100 focus:ring focus:ring-blue-200 transition duration-100 "
                name="tempat" id="tempat" placeholder="Tempat pekerjaan"></textarea>
-               <textarea class="form-textarea p-2 w-full border-gray-300 h-10 rounded-lg resize-none overflow-hidden focus:border-blue-100 focus:ring focus:ring-blue-200 transition duration-100 " 
+               <textarea class="form-textarea p-2 w-full border-gray-300 h-10 rounded-lg resize-none overflow-hidden focus:border-blue-100 focus:ring focus:ring-blue-200 transition duration-100 "
                name="gaji" id="gaji" placeholder="Gaji"></textarea>
-               <textarea class="form-textarea p-2 w-full border-gray-300 h-10 rounded-lg resize-none overflow-hidden focus:border-blue-100 focus:ring focus:ring-blue-200 transition duration-100 " 
+               <textarea class="form-textarea p-2 w-full border-gray-300 h-10 rounded-lg resize-none overflow-hidden focus:border-blue-100 focus:ring focus:ring-blue-200 transition duration-100 "
                name="pengalaman" id="pengalaman" placeholder="Pengalaman minimal "></textarea>
-              
+
              </div>
              <div class="text-right">
              <x-button>post</x-button>
              </div>
              </div>
-        </form>    
+        </form>
     </div>
 </x-container>
 
@@ -42,7 +42,7 @@
             </div>
             <div class="w-full">
             <div class="font-semibold">
-            {{ $loker->name }} 
+            {{ $loker->name }}
             </div>
             <div>
                 <div class=" text-sm text-blue-400">{{ $loker->bagian }}</div>
@@ -55,17 +55,17 @@
             <div class="text-sm text-right text-gray-500">{{ $loker->created_at->diffForhumans()}}</div>
             </div>
         </div>
-       
+
       </x-card>
-        
-        @endforeach  
-       
+
+        @endforeach
+
 
     </div>
-    {{-- <div class="mx-3 5"> 
+    {{-- <div class="mx-3 5">
         <div class="grid grid-cols-3 gap-6 space-y-3">
             <x-job :lokers="$lokers"/>
         </div>
    </div> --}}
-        
+
 </x-applayout>
